74af0e17b425f9878949f5808b49191894814747
CrazY <illie@yandex.ru>
2024-03-18 11:19:16 +0300

    test: adding sum button and result lineEdit

--- test/mainwindow.cpp
+++ test/mainwindow.cpp
@@ -19,3 +19,18 @@ void MainWindow::on_pushButton_clicked()
     this->close();
 }
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    bool ok = false;
+    int sum = ui->lineSummary->text().toInt(&ok);
+
+    if (ok) {
+        sum++;
+    } else {
+
+    }
+
+    ui->lineSummary->setText(QString::number(sum));
+}
+
